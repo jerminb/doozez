930c89c0f488007fafc66477b10120bc925b3983
Jermin Bazazian <jerminb@yahoo.com>
2021-06-26 14:12:04 +0100

    v1 api added. allowed hosts fixed to connect from emulator

--- doozez/settings.py
+++ doozez/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-elp26&n&p#ieq)x_06i-uqt%ohy5vkof#aqx%v4c!u!kzyn3ng
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1']
 
 
 # Application definition
